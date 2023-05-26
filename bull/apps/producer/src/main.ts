ee4b6c2b63f02c87bce57657bd30c46b06dd1dd9
philjung-jihyunlab <philjung.yun@gmail.com>
2023-05-26 21:39:25 +0900

    change swagger title

--- bull/apps/producer/src/main.ts
+++ bull/apps/producer/src/main.ts
@@ -4,7 +4,7 @@ import { ProducerModule } from './producer.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(ProducerModule, { cors: true });
-  const config = new DocumentBuilder().setTitle('JihyunLab Redis Microservice').setVersion('1.0').build();
+  const config = new DocumentBuilder().setTitle('JihyunLab Bull').setVersion('1.0').build();
 
   SwaggerModule.setup('api', app, SwaggerModule.createDocument(app, config));
   await app.listen(3001);
