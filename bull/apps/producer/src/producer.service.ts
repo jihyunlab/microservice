33e0139d58214cb828e219f027ff9f8645d4f91a
philjung-jihyunlab <philjung.yun@gmail.com>
2023-05-28 14:36:43 +0900

    change queue name

--- bull/apps/producer/src/producer.service.ts
+++ bull/apps/producer/src/producer.service.ts
@@ -4,12 +4,12 @@ import { Queue } from 'bull';
 
 @Injectable()
 export class ProducerService {
-  constructor(@InjectQueue('QUEUE') private userQueue: Queue) {}
+  constructor(@InjectQueue('QUEUE') private queue: Queue) {}
 
   async produce() {
     try {
       const message = String(Math.floor(Math.random() * (1000 - 1)) + 1);
-      this.userQueue.add('job', { message: message });
+      this.queue.add('job', { message: message });
 
       console.log(`produce: ${message}`);
     } catch (error) {
